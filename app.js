25582cf265323eb53368f4265088d67c04a1eb11
DavidTMaciel <davidmacieljr122@gmail.com>
2023-06-01 20:31:46 -0300

    Adicionando session e Middleware

--- app.js
+++ app.js
@@ -5,9 +5,24 @@
     const app = express();
     const admin = require('./routes/admin')
     const path = require('path');
-// const mongoose = require('mongoose');
+    const mongoose = require('mongoose');
+    const session = require('express-session');
+    const flash = require('connect-flash');
 
 //Configurações
+    //Sessão
+        app.use(session({
+            secret: "secret",
+            resave: true,
+            saveUninitialized: true
+        }))
+        app.use(flash());
+    //Middleware
+        app.use((req,res , next) =>{
+            res.locals.success_msg = req.flash('success_msg');
+            res.locals.error_msg = req.flash('error_msg');
+            next();
+        })
     //BodyParser
         app.use(bodyParser.urlencoded({extended: true}));
         app.use(bodyParser.json());
@@ -16,6 +31,18 @@
         app.set("view engine", "handlebars")
         app.set('views', 'views');
     //Mongose
+    mongoose.Promise = global.Promise
+    mongoose.connect(
+        process.env.MONGO_URI || "mongodb://0.0.0.0:27017/blogapp", 
+        {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        }).then( () => {
+            console.log("Connected to MongoDB");
+            
+        }).catch(function(erro){
+            console.log(erro);
+        }); 
 
     //Public
     app.use(express.static(path.join(__dirname, "public")));
--- routes/admin.js
+++ routes/admin.js
@@ -1,5 +1,8 @@
 const express = require('express')
 const router = express.Router();
+const mongoose =  require('mongoose');
+require("../models/Categorias")
+const Categoria = mongoose.model('Categorias');
 
 //Rota principal
     router.get('/', (req, res) => {
@@ -16,6 +19,18 @@ const router = express.Router();
     router.get('/categorias/add', (req, res) => {
         res.render("admin/addcategorias");
     });
+    router.post('/categorias/nova', (req, res) => {
+        const novaCategoria = {
+           nome: req.body.nome,
+           slug: req.body.slug
+        }
+        new Categoria(novaCategoria).save().then(() => {
+            console.log("Categoria salva com sucesso")
+        }).catch((erro) => {
+            console.log(erro)
+        }) 
+    });
+    
 
 
 module.exports = router;
