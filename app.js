9a66b0ef4840b7e9010af8fc9a65583131c2afb4
DavidTMaciel <davidmacieljr122@gmail.com>
2023-06-08 19:46:42 -0300

    Rotas da index categorias e postagens

--- app.js
+++ app.js
@@ -10,6 +10,8 @@
     const flash = require('connect-flash');
     require("./models/Postagens")
     const Postagem = mongoose.model('Postagens');
+    require("./models/Categorias");
+    const Categoria = mongoose.model('Categorias');
 
 //Configurações
     //Sessão
@@ -81,7 +83,33 @@
     })
 
     app.use('/admin', admin);
+    //Rota listagem categorias na pag inicial
+    app.get("/categorias", (req, res)=>{
+        Categoria.find().lean().then((categorias)=>{
+            res.render("categorias/index", {categorias: categorias});
+        }).catch((erro)=>{
+            req.flash('error_msg', "Houve um erro interno a listar as categorias");
+            res.redirect('/');
+        });
+    });
 
+    app.get("/categorias/:slug", (req, res)=>{
+        Categoria.findOne({slug: req.params.slug}).then((categoria)=>{
+            if(categoria){
+
+                Postagem.find({categoria: categoria._id}).lean().then((postagens)=>{
+                    res.render("categorias/postagens", {postagens: postagens, categoria: categoria})
+                });
+
+            }else{
+                req.flash('error_msg',"Essa categoria não existe.");
+                res.redirect('/');
+            };
+        }).catch((error)=>{
+            req.flash('error', "Houve um error interno a carregar a pagina");
+            res.redirect('/');
+        })
+    });
 //Outros
 const PORT = 8081;
 app.listen(PORT, () => {
