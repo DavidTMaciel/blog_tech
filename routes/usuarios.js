6e7c11fcd3ce483569f7bed67631af480099351e
DavidTMaciel <davidmacieljr122@gmail.com>
2023-06-08 20:37:36 -0300

    Exibição de erros do registro

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -18,15 +18,38 @@ router.post('/registro/novo', (req, res) => {
     //Validação
 
     if(!req.body.nome || req.body.nome == null || req.body.nome == undefined){
-        erros.push({Nome: "Nome invalido"})
+        erros.push({texto: "Nome invalido"})
     }
     if(!req.body.email || req.body.email == null || req.body.email == undefined){
-        erros.push({Nome: "Email invalido"})
+        erros.push({texto: "Email invalido"})
     }
-    if(!req.body.senha || req.body.senha == null || req.body.senha == undefined || req.body.senha <= 7){
-        erros.push({Nome: "Senha invalida"})
+    if(!req.body.senha || req.body.senha == null || req.body.senha == undefined || req.body.senha.length <= 7){
+        erros.push({texto: "Senha invalida"})
+    }
+    if(req.body.senha2 !== req.body.senha){
+        erros.push({texto: "As senhas não são iguais"})
+    }
+    if(erros.length > 0){
+        
+        res.render("usuarios/registro", {erros: erros})
     }
     else{
+        const novoUsuario = {
+
+            nome: req.body.nome,
+            email: req.body.email,
+            senha: req.body.senha
+
+        }
+        new Usuario(novoUsuario).save().then(() => {
+            req.flash("success_msg", "Usuario cadastrado com sucesso");
+            res.redirect("/admin")
+
+        }).catch((erros) =>{
+            req.flash("error_msg", "Ocorreu um erro ao registar o usuario, por favor tente novamente");;
+            res.redirect("usuarios/registro")
+
+        });
         
     }
 });
